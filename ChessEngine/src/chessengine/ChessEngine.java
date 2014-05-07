52182706c734977b479931277afb92032932b6e4
Rutuj <rutuj1729@gmail.com>
2014-05-08 01:04:21 +0530

    made skeleton

--- ChessEngine/src/chessengine/ChessEngine.java
+++ ChessEngine/src/chessengine/ChessEngine.java
@@ -16,13 +16,58 @@ public class ChessEngine {
     
 
     public static void main(String[] args) {
-        JFrame f=new JFrame("The Grandmaster");
+        /* JFrame f=new JFrame("The Grandmaster");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         UserInterface ui = new UserInterface();
         f.add(ui);
         f.setSize(500,500);
-        f.setVisible(true);
+        f.setVisible(true); */
+    }
+        
+        public static String posibleMoves() {
+            String list="";
+            for (int i=0; i<64; i++) {
+                switch (chessboard[i/8][i%8]) {
+                    case "P": list+=possibleP(i);
+                        break;
+                    case "R": list+=possibleR(i);
+                        break;
+                    case "N": list+=possibleN(i);
+                        break;
+                    case "B": list+=possibleB(i);
+                        break;
+                    case "Q": list+=possibleQ(i);
+                        break;
+                    case "K": list+=possibleK(i);
+                        break;
+            }
+        }
+        return list;//x1,y1,x2,yx,captured piece
+    }
+        public static String possibleP(int i) {
+            String list = "";
+            return list;
+        }
+        public static String possibleR(int i) {
+            String list = "";
+            return list;
+        }
+        public static String possibleN(int i) {
+            String list = "";
+            return list;
+        }
+        public static String possibleB(int i) {
+            String list = "";
+            return list;
+        }
+        public static String possibleQ(int i) {
+            String list = "";
+            return list;
+        }
+        public static String possibleK(int i) {
+            String list = "";
+            return list;
+        }
     }
     
-}
 
