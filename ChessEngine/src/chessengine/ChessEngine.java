fbb1894fdb7d541599297065a9db2a59b3104150
Rutuj <rutuj1729@gmail.com>
2014-05-08 02:30:28 +0530

    added basic king movement

--- ChessEngine/src/chessengine/ChessEngine.java
+++ ChessEngine/src/chessengine/ChessEngine.java
@@ -6,25 +6,41 @@ public class ChessEngine {
     static String chessboard[][] = {
         {"r","n","b","q","k","b","n","r"},
         {"p","p","p","p","p","p","p","p"},
-        {" "," "," "," "," "," "," "," "},
+        {" "," "," "," "," ","K"," "," "},
         {" "," "," "," "," "," "," "," "},
         {" "," "," "," "," "," "," "," "},
         {" "," "," "," "," "," "," "," "},
         {"P","P","P","P","P","P","P","P"},
-        {"R","N","B","Q","K","B","N","R"}};
+        {"R","N","B","Q"," ","B","N","R"}};
     
     
 
     public static void main(String[] args) {
+        
+        System.out.println(possibleMoves());
         /* JFrame f=new JFrame("The Grandmaster");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         UserInterface ui = new UserInterface();
         f.add(ui);
         f.setSize(500,500);
-        f.setVisible(true); */
+        f.setVisible(true);
+        1  9 17 25 33
+        2 10 18 26 ....
+        3 11 19 27
+        4 12 20 28
+        5 13 21 29
+        6 14 22 30 
+        7 15 23 31
+        8 16 24 32
+        
+        */
     }
         
-        public static String posibleMoves() {
+    /**
+     *
+     * @return
+     */
+    public static String possibleMoves() {
             String list="";
             for (int i=0; i<64; i++) {
                 switch (chessboard[i/8][i%8]) {
@@ -38,7 +54,8 @@ public class ChessEngine {
                         break;
                     case "Q": list+=possibleQ(i);
                         break;
-                    case "K": list+=possibleK(i);
+                    case "K": 
+                        list+=possibleK(i);
                         break;
             }
         }
@@ -65,9 +82,39 @@ public class ChessEngine {
             return list;
         }
         public static String possibleK(int i) {
-            String list = "";
+            String list = "", oldPiece;
+            int x=i/8, y = i%8;
+            for(int j = 0;j <= 8;j++)
+            {
+                
+                if(j!=4) {
+                    try {
+                    if (Character.isLowerCase(chessboard[x-1 + j/3][y-1 + j%3].charAt(0)) || " ".equals(chessboard[x-1 + j/3][y-1 + j%3])) {
+                        
+                        oldPiece = chessboard[x-1 + j/3][y-1 + j%3];
+                        chessboard[x-1 + j/3][y-1 + j%3] = "K";
+                        if(true) {
+                            list = list + x + y + (x-1 + j/3) + (y-1 + j%3) + oldPiece;                          
+                        }
+                        chessboard[x-1 + j/3][y-1 + j%3] = oldPiece;
+                        chessboard[x][y] = "K";
+                        
+                    } 
+                    }
+                    catch(Exception e) {
+                    }
+                    }
+                        
+            }
             return list;
         }
+
+    /* private static boolean kingSafe() {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        }*/
+        
     }
     
 
